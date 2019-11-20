a2c1928b284623e99376aed5c9a33593d878f0f8
Otto Hyytiälä <otto.hyytiala@gmail.com>
2019-11-20 22:37:32 +0200

    send directly to another node

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -1,10 +1,11 @@
 const messagesRouter = require('express').Router()
 const fetch = require('node-fetch')
 const messages = []
+const nodeList = String(process.env.NODE_URLS).split(';')
 
 messagesRouter.get('/', async (req, res) => {
   try {
-    const nodeUrls = String(process.env.NODE_URLS).split(';').map(addr => addr.trim() + '/messages/state')
+    const nodeUrls = nodeList.map(addr => addr.trim() + '/messages/state')
     const requests = []
     nodeUrls.forEach(url => {
       requests.push(fetch(url).then(res => res.json()))
@@ -25,6 +26,12 @@ messagesRouter.get('/state', (req, res) => {
   res.status(200).json(messages)
 })
 
+messagesRouter.post('/receive', (req, res) => {
+  const newMessage = req.body
+  messages.push(newMessage)
+  res.sendStatus(200)
+})
+
 messagesRouter.post('/', async (req, res) => {
   if (req.body.name === undefined || req.body.message === undefined) {
     res.sendStatus(400)
@@ -35,9 +42,19 @@ messagesRouter.post('/', async (req, res) => {
       name: req.body.name,
       message: req.body.message
     }
-    messages.push(message)
+    if (req.query && req.query.to) {
+      const address = nodeList[req.query.to].trim() + '/messages/receive'
+      await Promise.resolve(fetch(address, {
+        method: 'post',
+        body: JSON.stringify(message),
+        headers: { 'Content-Type': 'application/json' }
+      }))
+    } else {
+      messages.push(message)
+    }
     res.sendStatus(201)
   } catch (error) {
+    console.log(error)
     res.sendStatus(500)
   }
 })
